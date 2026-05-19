a45571249635a65b43221de6433f744d4db939f6
mdyasinreza1510 <aec.cse.2024mdyasinreza@gmail.com>
2026-05-19 23:05:18 +0530

    complted  a basic layout logic are not done yet

---
+++ src/Todolist.jsx
@@ -0,0 +1,59 @@
+import { useState } from 'react'
+function Todolist(){
+
+    const [tasks, setatsk]=useState(["eat","sleep", "code","repeat"]);
+    const [newtask,setnewtask]=useState("");
+
+    function inputchange(event){
+       setnewtask( event.target.value);
+
+    }
+
+    function addtask(){
+
+    }
+    function dlttask(index){
+        
+    }
+    function movetaskup(){
+        
+    }
+    function movetaskdown(index){
+        
+    }
+    return(
+<>
+<div className="main-container">
+    
+<div className='heading'>
+    <h1>TO-DO-LIST</h1>
+</div>
+<div className='input-box'>
+    <input 
+    type="text"
+    placeholder='Enter new task....' 
+    value={newtask}
+    onChange={inputchange}/>
+    <button className='adbtn'
+    onClick={addtask}>Add</button>
+</div>
+<div className='list-box'>
+    <ol>
+        {tasks.map((task, index)=>
+        <li key={index}>
+            <span>{task}</span>
+            <button onClick={dlttask}>Delet</button>
+            <button onClick={movetaskup}>Up</button>
+            <button onClick={movetaskdown}>Down</button>
+
+        </li>
+        )}
+    </ol>
+</div>
+
+</div>
+
+</>
+    );
+}
+export default Todolist
